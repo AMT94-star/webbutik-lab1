7f725c84060e22451ef82eae328a56c55a9ad18c
AMT94-star <aslihan.taskin94@gmail.com>
2026-08-25 11:42:37 +0200

    added cart functionality

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+const cartItems = {};
+const cartItemContainer = document.getElementById("cartItems");
 const buyButtons = document.querySelectorAll(".buy-button");
+
+const updateCart = () => {
+  cartItemContainer.innerHTML = "";
+
+  //hämtar alla nycklar dvs produkter från objekten/cart
+  if (Object.keys(cartItems).length == 0) {
+    cartItemContainer.textContent = "Varukorgen är tom";
+    return;
+  }
+
+  for (const name in cartItems) {
+    const amount = cartItems[name];
+
+    //skapar en ny p-tagg
+    const item = document.createElement("p");
+    item.textContent = `${name}: ${amount} st`;
+
+    cartItemContainer.appendChild(item);
+  }
+};
+
+updateCart();
 
 buyButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const name = button.dataset.name;
     const productCard = button.closest(".product-card");
     const quantityAdd = productCard.querySelector(".quantity");
-    const amount = quantityAdd.value;
+    const amount = Number(quantityAdd.value);
 
-    alert(`${amount} st av varan ${name} lagd i varukorgen`);
+    if (cartItems[name]) {
+      cartItems[name] += amount;
+    } else {
+      cartItems[name] = amount;
+    }
+
+    updateCart();
+    //alert(`${amount} st av varan ${name} lagd i varukorgen`);
   });
 });
 
